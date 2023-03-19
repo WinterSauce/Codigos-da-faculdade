360a5c49bcb0d6d872099c668bcea91e1aea0ae5
GeneralInverno1945 <114249305+GeneralInverno1945@users.noreply.github.com>
2023-03-19 17:48:03 -0300

    Add files via upload

---
+++ Atividade_5.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.Scanner;
+public class Atividade_5 {
+    static public void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        int valor;
+
+        System.out.println("Digite um valor: ");
+        valor = sc.nextInt();
+
+        if (valor > 0){
+            System.out.println(valor = valor * valor);
+        } else{
+            System.out.println(valor = valor * valor * valor);
+        }
+
+
+
+
+
+
+
+    }
+}
